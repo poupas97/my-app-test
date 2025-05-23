abaa5eac42da55c62728b937ea1843fc49c310f6
Ruben Carreira <r.carreira@Mac.Home>
2025-05-23 11:34:06 +0100

    remove

--- src/request.ts
+++ src/request.ts
@@ -1,17 +1,3 @@
-import axios from "axios";
-
-const config = {
-  baseURL: "https://dev.checks-api.putzmeister.com/api",
-  headers: {
-    common: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  },
-};
-
-export const api = axios.create(config);
-
 export const makeApiRequest = async <D = any, P = Record<string, unknown>>(
   path: string,
   method = "GET",
